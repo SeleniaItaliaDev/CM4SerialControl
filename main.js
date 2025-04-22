acc869f1c9469dc510f887f2281b00bd9a8269d2
Al366io <alessio.nannipieri0@gmail.com>
2025-04-22 18:03:24 +0200

    ESM stuff

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import { app, BrowserWindow, ipcMain } from 'electron';
-import { join } from 'path';
+import path, { join } from 'path';
 import { sendLedCommand } from './serial.js';
 import { fileURLToPath } from 'url';
+
+// ESM __dirname workaround
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -10,7 +12,7 @@ function createWindow() {
     width: 400,
     height: 300,
     webPreferences: {
-        preload: join(__dirname, 'preload.js'),
+      preload: join(__dirname, 'preload.js'),  // ✅ ESM-safe preload path
     }
   });
 
@@ -18,6 +20,7 @@ function createWindow() {
 }
 
 app.whenReady().then(createWindow);
+
 
 ipcMain.handle('led-control', async (_, on) => {
   return await sendLedCommand(on);
