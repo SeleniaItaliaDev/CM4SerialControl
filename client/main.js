c099ca7b08dcf35fe8df06f3e04323362dea2e80
Al366io <alessio.nannipieri0@gmail.com>
2025-04-30 17:09:09 +0200

    wip

--- client/main.js
+++ client/main.js
@@ -1,34 +1,16 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
-const isDev = !app.isPackaged;
-
 function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 400,
+    height: 300,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'), // optional
-      nodeIntegration: false,
-      contextIsolation: true,
-    },
+    }
   });
 
-  const url = isDev
-    ? 'http://localhost:5173'
-    : `file://${path.join(__dirname, '../client-ui/dist/index.html')}`;
-
-  win.loadURL(url);
+  win.loadFile('index.html');
 }
 
-app.whenReady().then(() => {
-  createWindow();
-
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
-});
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit();
-});
+app.whenReady().then(createWindow);
