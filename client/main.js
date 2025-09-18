dafbada9fc4f79faca3ab4d25bfe5cfecd8a15f0
Al366io <alessio.nannipieri0@gmail.com>
2025-09-18 16:53:15 +0200

    cleaning and actual react+electro setup

--- client/main.js
+++ client/main.js
@@ -1,17 +1,22 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+const isDev = process.env.NODE_ENV === 'development';
 
-function createWindow() {
+function createWindow () {
   const win = new BrowserWindow({
-    width: 400,
-    height: 300,
+    width: 800,
+    height: 600,
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
     }
   });
 
-  win.loadFile(path.join(__dirname, 'public/index.html'));
+  if (isDev) {
+    win.loadURL('http://localhost:5173'); // Vite dev server
+  } else {
+    win.loadFile(path.join(__dirname, 'serialcontrol-ui/dist/index.html')); // Production build
+  }
 }
 
 app.commandLine.appendSwitch('disable-gpu');
